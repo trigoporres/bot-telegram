7a1926ca5db0d8c8fe107ff86cf4d6fdf1e1e1ea
trigoporres <santiagotrigoporres@gmail.com>
2018-02-07 20:33:59 +0100

    hacia atras

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const TelegramBot = require('node-telegram-bot-api');
 const request = require('request');
 require('dotenv').config()
 
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
-
 const token = process.env.token_bot;
 const bot   = new TelegramBot(token, {polling: true});
 
