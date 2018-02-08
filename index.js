724a2cc66bf3f3ee147aadd481a65f33d4290541
trigoporres <santiagotrigoporres@gmail.com>
2018-02-08 15:11:49 +0100

    deploy

--- index.js
+++ index.js
@@ -8,14 +8,7 @@ const bot   = new TelegramBot(token, {polling: true});
 //require('./msgSystem')(bot)
 require('./emt.js')(bot)
 
-exports.helloHttp = function helloHttp (req, res) {
-  response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
-  
-  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-  res.send(JSON.stringify({ "speech": response, "displayText": response 
-  //"speech" is the spoken version of the response, "displayText" is the visual version
-  }));
-};
+
 
 
 
