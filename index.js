b1400ef6dde96bf5005776dafc2dc1077de81c97
trigoporres <santiagotrigoporres@gmail.com>
2018-02-08 13:43:42 +0100

    deploy

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ require('./emt.js')(bot)
 
 exports.helloHttp = function helloHttp (req, res) {
   response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
-
-
+  
   res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
   res.send(JSON.stringify({ "speech": response, "displayText": response 
   //"speech" is the spoken version of the response, "displayText" is the visual version
