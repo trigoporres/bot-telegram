d9687e5107c5e3d1d0bec73482a518b175eead52
trigoporres <santiagotrigoporres@gmail.com>
2018-06-18 13:55:45 +0200

    añadido kyogre

--- index.js
+++ index.js
@@ -8,3 +8,37 @@ const bot   = new TelegramBot(token, {polling: true});
 //require('./msgSystem')(bot)
 require('./emt.js')(bot)
 
+const kyogre = [
+  {
+    cp: '2328',
+    porcentaje: '100%'
+  },
+  {
+    cp: '2323',
+    porcentaje: '98%'
+  }
+]
+
+bot.on('message', (msg) => {
+  console.log(msg)
+  if(msg.text.includes("Kyogre")){
+    const chatId = msg.chat.id;
+    const info = "¿Esta potenciado?"
+    bot.sendMessage(chatId, info)
+    bot.on('message', (msg) => {
+      if(msg.text == "Si"){
+        const cp = "¿cual es su cp?"
+        bot.sendMessage(chatId, cp)
+      }
+    })
+    bot.on('message', (msg) => {
+      kyogre.forEach( element => {
+        console.log(element)
+        if(msg.text == element.cp){
+          const result = "tu kyogre es de" + element.porcentaje
+          bot.sendMessage(chatId, result)
+        }
+      })
+    })
+  }
+})
